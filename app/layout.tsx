a85021ae511683bee3270cd33d3fcf7dad0c9c39
M Umar <umarabdulhaque@gmail.com>
2026-03-05 16:51:42 +0500

    feat:added prisma file and everything in this project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import QueryProvider from "@/lib/providers/QueryProvider";
 import "./globals.css";
 
 const inter = Inter({
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} font-sans antialiased`}>
-        {children}
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
