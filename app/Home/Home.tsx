0cae7e0c0ef0c8996684758de1c0590b2921798e
Muhammad Annas Khan <annasking601@gmail.com>
2026-03-05 17:00:17 +0500

    .

--- app/Home/Home.tsx
+++ app/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Cards from '@/components/home/Cards'
 import Hero from '@/components/home/Hero'
 import Navbar from '@/components/home/Navbar'
 
@@ -6,6 +7,7 @@ const page = () => {
         <div className="min-h-screen bg-background">
             <Navbar />
             <Hero />
+            <Cards />
         </div>
     )
 }
